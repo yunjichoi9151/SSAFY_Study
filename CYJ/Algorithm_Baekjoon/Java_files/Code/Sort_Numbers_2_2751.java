fef54086f412ed63762d7871ce44606ef1ef743c
yunjichoi9151 <cyjcode99@gmail.com>
2022-08-01 11:04:23 +0900

    Add 2751 file

---
+++ CYJ/Algorithm_Baekjoon/Java_files/Code/Sort_Numbers_2_2751.java
@@ -0,0 +1,21 @@
+package Java_files.Code;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Sort_Numbers_2_2751 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        ArrayList<Integer> numlist = new ArrayList<>();
+        for(int i = 0; i < n; i++)
+            numlist.add(sc.nextInt());
+        Collections.sort(numlist);
+        StringBuilder sb = new StringBuilder();
+		for(int i : numlist)
+			sb.append(i).append('\n');
+		System.out.println(sb);
+        sc.close();
+    }
+}
