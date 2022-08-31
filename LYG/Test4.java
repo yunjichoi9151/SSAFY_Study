71b99277b9a2069eb98c7e8c7e204825afb68b66
Lee YunGyung <yxxnkxx@gmail.com>
2022-08-31 13:23:12 +0900

    pull test2

--- LYG/Test4.java
+++ LYG/Test4.java
@@ -1,12 +1,13 @@
 
-
-public class BOJ_14696_딱지 {
+public class Test {
 	public static void main(String[] args) {
 		
 		System.out.println("Test");
+		
+		// 줄을 선택해서
 		System.out.println("Test2");
+		// 리뷰를 달아보세요..
 		System.out.println("Test3");
 		System.out.println("Test4");
-		System.out.println("Test5");		
 	}
 }
