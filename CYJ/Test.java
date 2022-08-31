86315bb899e90f49e0abbb438296f1d5cccffc3b
yunjichoi9151 <cyjcode99@gmail.com>
2022-08-31 13:10:38 +0900

    Add Test.java

---
+++ CYJ/Test.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+    }
+}
