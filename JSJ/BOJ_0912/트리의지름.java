dae952945f2066f86a5ee2fa673248814f38f72a
조수정 <suz.dev33@gmail.com>
2022-09-12 21:06:32 +0900

    220912

--- JSJ/BOJ_0912/트리의지름.java
+++ JSJ/BOJ_0912/트리의지름.java
@@ -3,68 +3,70 @@ package BOJ_0912;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-// 시간초과
 public class 트리의지름 {
 
-	static int V;
-	static ArrayList<Node>[] adj; // 인접리스트
-	static boolean[] visit;
-	static int max;
+	static ArrayList<Node>[] adj;
+	static boolean[] visited;
+	static int max = 0;
+	static int node;
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String args[]) {
+		Scanner scan = new Scanner(System.in);
 
-		V = sc.nextInt(); // 트리 정점의 개수
-
+		int V = scan.nextInt();
 		adj = new ArrayList[V + 1];
-		for (int i = 1; i <= V; i++) {
-			adj[i] = new ArrayList<Node>();
+		for (int i = 1; i < V + 1; i++) {
+			adj[i] = new ArrayList<>();
 		}
 
 		for (int i = 0; i < V; i++) {
-			int n1 = sc.nextInt(); // 정점 번호
-
+			int n1 = scan.nextInt();
 			while (true) {
-				int n2 = sc.nextInt(); // 연결된 정점 번호
-				if (n2 == -1) // 종료 조건
+				int n2 = scan.nextInt();
+				if (n2 == -1)
 					break;
-				int d = sc.nextInt(); // 거리
-
+				int d = scan.nextInt();
 				adj[n1].add(new Node(n2, d));
-				adj[n2].add(new Node(n1, d));
 			}
 		}
 
-		max = 0;
-		for (int i = 1; i <= V; i++) {
-			visit = new boolean[V + 1];
-			if (!visit[i]) {
+		// 임의의 노드(1)에서 부터 가장 먼 노드를 찾는다. 이때 찾은 노드를 node에 저장한다.
+		visited = new boolean[V + 1];
+		dfs(1, 0);
 
-				dfs(i, 0);
-			}
-		}
+		// node에서 부터 가장 먼 노트까지의 거리를 구한다.
+		visited = new boolean[V + 1];
+		dfs(node, 0);
 
-		// 출력
 		System.out.println(max);
 	}
 
-	private static void dfs(int num, int dim) {
-		visit[num] = true;
+	public static void dfs(int num, int dim) {
 
-		for (Node node : adj[num]) {
-			if (!visit[node.num]) {
-				dfs(node.num, dim + node.len);
+		// 최대값 갱신
+		if (dim > max) {
+			max = dim;
+			node = num;
+		}
+
+		visited[num] = true;
+
+		for (int i = 0; i < adj[num].size(); i++) {
+			Node node = adj[num].get(i);
+			if (visited[node.num] == false) {
+				dfs(node.num, node.dim + dim);
+				visited[node.num] = true;
 			}
 		}
-		max = Math.max(max, dim);
 	}
 
-	static class Node {
-		int num, len;
+	public static class Node {
+		int num;
+		int dim;
 
-		public Node(int num, int len) {
+		public Node(int num, int dim) {
 			this.num = num;
-			this.len = len;
+			this.dim = dim;
 		}
 	}
 }
